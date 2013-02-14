40f81ded4659233a1a1c4396592f041de4226292
compwron <wordymusic@gmail.com>
2013-02-14 13:13:05 -0600

    &L: refactoring and fixing connectionUtil to get actual html. regex is still broken

--- src/AirportCodeHelper.java
+++ src/AirportCodeHelper.java
@@ -1,35 +1,32 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AirportCodeHelper {
     List<String> airPortCodes;
 
     public AirportCodeHelper(){
+        String longListOfStationCodes = "AVX\n" +
+                "AWG\n" +
+                "AXA\n" +
+                "AXN\n" +
+                "AYS\n" +
+                "AZO\n" +
+                "B23\n" +
+                "BAB\n" +
+                "BAD\n" +
+                "BAF\n";
         this.airPortCodes = parseAirportCodes(longListOfStationCodes);
     }
 
     List<String> parseAirportCodes(String airportCodes) {
         List<String> codeList = new ArrayList<String>();
-
         String[] codes = airportCodes.split("\n");
-        for(String airportCode : codes){
-            codeList.add(airportCode);
-        }
+        Collections.addAll(codeList, codes);
         return codeList;
     }
 
     public List<String> getAirportCodes() {
         return airPortCodes;
     }
-
-    String longListOfStationCodes = "AVX\n" +
-            "AWG\n" +
-            "AXA\n" +
-            "AXN\n" +
-            "AYS\n" +
-            "AZO\n" +
-            "B23\n" +
-            "BAB\n" +
-            "BAD\n" +
-            "BAF\n";
 }
--- src/ConnectUtil.java
+++ src/ConnectUtil.java
@@ -1,6 +1,6 @@
-import sun.net.www.http.HttpClient;
-
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -17,10 +17,22 @@ public class ConnectUtil {
     String htmlFrom(String url){
         try {
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            return connection.getResponseMessage();
+            String response = readString(connection.getInputStream());
+            return response;
         } catch (IOException e) {
             e.printStackTrace();
+            return "";
         }
-        return "";
+    }
+
+    // http://stackoverflow.com/questions/1763789/how-do-i-convert-an-inputstream-to-a-string-in-javas
+    private static String readString(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream into = new ByteArrayOutputStream();
+        byte[] buf = new byte[4096];
+        for (int n; 0 < (n = inputStream.read(buf));) {
+            into.write(buf, 0, n);
+        }
+        into.close();
+        return new String(into.toByteArray(), "UTF-8"); // Or whatever encoding
     }
 }
